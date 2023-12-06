a3a456fe0141e77b3757f37e3a62f06ca9cbfe4a
nukkua <marco.salazar.2014.com@gmail.com>
2023-12-05 22:39:41 -0400

    f search and lazyloading functions

--- src/components/shared/navbar/navbar.tsx
+++ src/components/shared/navbar/navbar.tsx
@@ -1,6 +1,7 @@
 import { component$ } from "@builder.io/qwik";
-import { QwikLogo } from "../icons/qwik";
-import styles from "./header.module.css";
+import { QwikLogo } from "../../icons/qwik";
+
+import styles from "./navbar.module.css";
 
 export default component$(() => {
   return (
@@ -8,7 +9,7 @@ export default component$(() => {
       <div class={["container", styles.wrapper]}>
         <div class={styles.logo}>
           <a href="/" title="qwik">
-            <QwikLogo height={50} width={143} />
+            <QwikLogo height={50} />
           </a>
         </div>
         <ul>
@@ -26,14 +27,6 @@ export default component$(() => {
               target="_blank"
             >
               Examples
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://qwik.builder.io/tutorial/welcome/overview/"
-              target="_blank"
-            >
-              Tutorials
             </a>
           </li>
         </ul>
